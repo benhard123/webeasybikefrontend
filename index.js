6be5784aa36d64938ee4280f961cb68ef8d4a7e8
Benhard Tampubolon <benhard_tampubolon23@yahoo.com>
2021-02-13 22:11:07 +0700

    bike location update

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 let map;
-var host = "http://172.17.0.5"
+let bikelocation = [];
 
 initial()
+setInterval(lokasiSepeda,2000)
 
 window.addEventListener('popstate', function(event) {
   initial()
@@ -19,6 +20,7 @@ function initial(){
   if(window.location.pathname == "/" ){
     document.getElementsByTagName("title")[0].innerText = "Easy Bike Unpad"
     loadDoc(fungsi=untukMap)
+    lokasiSepedaInit();
   }
   else if(window.location.pathname == "/login/"){
     document.getElementsByTagName("title")[0].innerText = "Easy Bike Unpad - Login"
@@ -47,7 +49,11 @@ function loadDoc(fungsi=null, content = "content.html?dev="+ Math.floor(Math.ran
   xhttp.send();
   // fetch("content.html").then(
   //   response=>{
-  //     document.getElementById("content").innerHTML = response;
+  //     let data=""
+  //     for(i=0; i< response.length; i++){
+  //       data+=response[i]
+  //     }
+  //     document.getElementById("content").innerHTML = data;
   //     fungsi();
   //   }
   // )
@@ -78,42 +84,6 @@ function untukMap(){
     },
     {
       position: new google.maps.LatLng(-6.927027, 107.773170),
-      type: "info",
-    },
-    {
-      position: new google.maps.LatLng(-33.91747, 151.22912),
-      type: "info",
-    },
-    {
-      position: new google.maps.LatLng(-33.9191, 151.22907),
-      type: "info",
-    },
-    {
-      position: new google.maps.LatLng(-33.91725, 151.23011),
-      type: "info",
-    },
-    {
-      position: new google.maps.LatLng(-33.91872, 151.23089),
-      type: "info",
-    },
-    {
-      position: new google.maps.LatLng(-33.91784, 151.23094),
-      type: "info",
-    },
-    {
-      position: new google.maps.LatLng(-33.91682, 151.23149),
-      type: "info",
-    },
-    {
-      position: new google.maps.LatLng(-33.9179, 151.23463),
-      type: "info",
-    },
-    {
-      position: new google.maps.LatLng(-33.91666, 151.23468),
-      type: "info",
-    },
-    {
-      position: new google.maps.LatLng(-33.916988, 151.23364),
       type: "info",
     },
     {
@@ -165,22 +135,99 @@ function untukMap(){
     })
   }
 
-  const triangleCoords = [
-    { lat: -6.919095, lng: 107.768934 },
-    { lat: -6.923578, lng: 107.776512 },
-    { lat: -6.929503, lng: 107.773546 },
-    { lat: -6.918095, lng: 107.767934  },
+  const koordinatgeofence = [
+    { lat: -6.930246, lng: 107.774365 },
+    { lat: -6.928944, lng: 107.777785 },
+    { lat: -6.919930, lng: 107.774055 },
+    { lat: -6.921711, lng: 107.769723 },
+    { lat: -6.930246, lng: 107.774365  },
   ];
-  
-  const bermudaTriangle = new google.maps.Polygon({
-    paths: triangleCoords,
+  const geofence = new google.maps.Polygon({
+    paths: koordinatgeofence,
     strokeColor: "#FF0000",
     strokeOpacity: 0.8,
     strokeWeight: 2,
     fillColor: "#FF0000",
     fillOpacity: 0.35,
   });
-  bermudaTriangle.setMap(map);
+  geofence.setMap(map);
+}
+
+function lokasiSepeda(){
+  const iconBase =
+    "https://developers.google.com/maps/documentation/javascript/examples/full/images/";
+    const icons = {
+      parking: {
+        icon: iconBase + "parking_lot_maps.png",
+      },
+      library: {
+        icon: iconBase + "library_maps.png",
+      },
+      info: {
+        icon: iconBase + "info-i_maps.png",
+      },
+    };
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      let hasil = JSON.parse(this.responseText)
+      console.log(JSON.parse(this.responseText))
+      for (let i =0; i<hasil.length; i++){
+        bikelocation[i].setPosition(new google.maps.LatLng(hasil[i].latitude,hasil[i].longitude))
+      }
+    }
+    // else if(this.readyState != 4){
+    //   document.getElementById("content").innerHTML = "loading"
+    // }
+    // else{
+    //   document.getElementById("content").innerHTML = "error";
+    // }
+  };
+  xhttp.open("GET", "http://172.17.0.2:8000/gpsdata", true);
+  xhttp.setRequestHeader("Accept", "application/json");
+  if(window.location.pathname == "/"){
+    xhttp.send();
+  }
+}
+
+function lokasiSepedaInit(){
+  const iconBase =
+    "https://developers.google.com/maps/documentation/javascript/examples/full/images/";
+    const icons = {
+      parking: {
+        icon: iconBase + "parking_lot_maps.png",
+      },
+      library: {
+        icon: iconBase + "library_maps.png",
+      },
+      info: {
+        icon: iconBase + "info-i_maps.png",
+      },
+    };
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      let hasil = JSON.parse(this.responseText)
+      console.log(JSON.parse(this.responseText))
+      for (let i =0; i<hasil.length; i++){
+        bikelocationok = new google.maps.Marker({
+          position: new google.maps.LatLng(hasil[i].latitude,hasil[i].longitude),
+          icon: icons["library"].icon,
+          map: map,
+        });
+        bikelocation.push(bikelocationok);
+      }
+    }
+    // else if(this.readyState != 4){
+    //   document.getElementById("content").innerHTML = "loading"
+    // }
+    // else{
+    //   document.getElementById("content").innerHTML = "error";
+    // }
+  };
+  xhttp.open("GET", "http://172.17.0.2:8000/gpsdata", true);
+  xhttp.setRequestHeader("Accept", "application/json");
+  xhttp.send();
 }
 
 function initMap() {
@@ -188,5 +235,19 @@ function initMap() {
 }
 
 function loginfunction(){
-  alert(document.getElementsByName("username")[0].value + document.getElementsByName("password")[0].value)
+  var xhttp = new XMLHttpRequest();
+  // xhttp.responseType = "json"
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      alert(JSON.stringify(JSON.parse(this.responseText)));
+    }
+    else if(this.readyState == 4){
+      alert(this.responseText);
+    }
+  };
+  xhttp.open("POST", "http://172.17.0.2:8000/login", true);
+  xhttp.setRequestHeader("Content-Type", "application/json");
+  xhttp.setRequestHeader("Accept", "application/json")
+  xhttp.send(JSON.stringify({username: document.getElementsByName("username")[0].value, password: document.getElementsByName("password")[0].value}));
+  // alert(document.getElementsByName("username")[0].value + document.getElementsByName("password")[0].value)
 }
